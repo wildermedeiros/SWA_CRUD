5a46c61d80249ed1d90de3a6aeb13b18b5cfadc3
wildermedeiros <wildersiqueiramedeiros@gmail.com>
2023-09-22 14:57:41 -0300

    Text edit for date added and working on CRUD events functions

--- Task.gd
+++ Task.gd
@@ -1,7 +1,18 @@
 extends Button
 
+@export var desc_label: Label
+@export var date_label: Label
 
 @onready var parent = get_parent()
 
 func _on_pressed():
 	parent.task_on_focus = self
+	parent.set_desc_edit(desc_label.text)
+	parent.set_date_edit(date_label.text)
+
+func set_desc_label(new_desc):
+	desc_label.text = new_desc
+
+func set_date_label(new_date):
+	date_label.text = new_date
+
--- TaskList.gd
+++ TaskList.gd
@@ -3,8 +3,11 @@ extends VBoxContainer
 @export var delete_button: Button
 @export var edit_button: Button
 @export var task_scene: PackedScene
+@export var desc_edit: TextEdit
+@export var date_edit: TextEdit
 
 var tasks = []
+
 var task_on_focus = null:
 	get: return task_on_focus
 	set(value): 
@@ -12,7 +15,7 @@ var task_on_focus = null:
 		#emit task_on_focus change
 	
 func _process(delta):
-	#Melhorar isso aqui, talvez colocar em um evento, quando há mudança no taskonfocus
+	#Melhorar isso aqui, talvez colocar em um evento, quando há mudança no task_on_focus
 	delete_button.disabled = task_on_focus == null
 	edit_button.disabled = task_on_focus == null
 
@@ -23,11 +26,33 @@ func _on_delete_task():
 		if tasks.has(node_to_delete):
 			tasks.erase(node_to_delete)
 			node_to_delete.queue_free()
+			clear_edit_texts()
 
 func _on_create_task_button_down():
 	var task = task_scene.instantiate()
+	update_task_label(task)
+	clear_edit_texts()
 	tasks.append(task)
 	add_child(task)
 
 func _on_edit_task_button():
-	pass
+	if task_on_focus != null:
+		var task_path = task_on_focus.get_path()
+		var task_to_edit = get_node(task_path)
+		update_task_label(task_to_edit)
+		clear_edit_texts()
+		task_on_focus = null
+
+func update_task_label(task):
+	task.set_desc_label(desc_edit.text)
+	task.set_date_label(date_edit.text)
+		
+func set_desc_edit(new_text):
+	desc_edit.text = new_text
+
+func set_date_edit(new_text):
+	date_edit.text = new_text
+
+func clear_edit_texts():
+	desc_edit.text = ""
+	date_edit.text = ""
